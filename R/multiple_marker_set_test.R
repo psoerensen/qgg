0a85e253902b8eb088bb52c8f59a8920d4005a5a
psoerensen <pso@mbg.au.dk>
2017-10-27 12:45:02 +0200

    Split into separate files

---
+++ R/multiple_marker_set_test.R
@@ -0,0 +1,254 @@
+####################################################################################################################
+#    Module 2: SetTests
+####################################################################################################################
+#' 
+#' Genetic marker set tests based on sum statistics
+#'
+#' @description
+#' Set test based on summing the single genetic marker test statistics.
+#' The sum test is powerful if the genomic feature harbors many genetic markers having small to moderate effects. 
+#'                       
+#' @details
+#' The singler marker test statistics can be obtained from GBLUP and GFBLUP model fits or from standard GWAS. 
+#' The distribution of this test statistic under the null hypothesis (associated markers are picked at random from the total 
+#' number of tested genetic markers) is difficult to describe in terms of exact or approximate 
+#' distributions, and an empirical distribution is required.
+#'                        
+#' @param stat vector of single marker statistics (e.g. marker effects, t-stat, p-values)
+#' @param sets list of marker sets - names corresponds to rownames in stat
+#' @param nperm number of permutations
+#' @param W matrix of centered and scaled genotypes (used if method = cvat or score)
+#' @param method including sum, cvat, hyperG, score
+#' @param threshold used if method = hyperG
+#' @return Returns a dataframe including 
+#' \item{setT}{marker set test statistics} 
+#' \item{nset}{number of markers in the set}
+#' \item{p}{p-value for marker set}
+#' @author Peter Sørensen
+#’ @references Rohde, P. D., Demontis, D., Cuyabano, B. C. D., Børglum, A. D., & Sørensen, P. (2016). Covariance Association Test (CVAT) Identifies Genetic Markers Associated with Schizophrenia in Functionally Associated Biological Processes. Genetics, 203(4), 1901-1913.
+#’ @references Rohde, P. D., Edwards S. M., Sarup P., Sørensen, P. (August, 2014). Gene-based Association Approach Identify Genes Across Stress Traits in Fruit Flies. Poster presented at the 10th World Congress of Genetics Applied to Livestock Production (WCGALP), Vancouver, Canada.
+#' @examples
+#' 
+#' # Simulate data
+#' W <- matrix(rnorm(2000000), ncol = 10000)
+#'   colnames(W) <- as.character(1:ncol(W))
+#' y <- rowSums(W[, 1:10]) + rowSums(W[, 1001:1010]) + rnorm(nrow(W))
+#' 
+#' # REML analyses 
+#' data <- data.frame(y = y, mu = 1)
+#' fm <- y ~ mu
+#' fit <- gfm(fm = fm, W = W, sets = list(colnames(W)), data = data)
+#' fit$df <- 10
+#' fit$p <- pt(fit$s / sqrt(fit$vs), df = fit$df, lower.tail = FALSE) 
+#' 
+#' sets <- list(A = as.character(1:100), B = as.character(101:1000), C = as.character(1001:5000), D = as.character(5001:10000))
+#'
+#' # Set test based on sums 
+#' res <- setTest(stat = fit$s**2, sets = sets, method = "sum", nperm = 100)
+#' 
+#' # Set test based on cvat 
+#' res <- setTest(stat = fit$s, W = W, sets = sets, method = "cvat", nperm = 100)
+#' 
+#' # Set test based on hyperG 
+#' res <- setTest(stat = fit$p, sets = sets, method = "hyperG", threshold = 0.05)
+#' 
+#' @export
+#'
+
+setTest <- function(stat = NULL, W = NULL, sets = NULL, nperm = NULL, method = "sum", threshold = 0.05) {
+     
+  if (method == "sum") setT <- sumTest(stat = stat, sets = sets, nperm = nperm) 
+  if (method == "cvat") setT <- cvat(s = stat, W = W, sets = sets, nperm = nperm) 
+  if (method == "hyperG") setT <- hgTest(p = stat, sets = sets, threshold = threshold) 
+  if (method == "score") setT <- scoreTest(e = e, W = W, sets = sets, nperm = nperm)
+     
+  return(setT)
+
+}
+
+sumTest <- function(stat = NULL, sets = NULL, nperm = NULL, method = "sum") {
+     
+  if (method == "mean") setT <- sapply(sets, function(x) {mean(stat[x])})
+  if (method == "sum") setT <- sapply(sets, function(x) {sum(stat[x])})
+  if (method == "max") setT <- sapply(sets, function(x) {max(stat[x])})
+  if (!is.null(nperm)) {
+    p <- rep(0, length(sets)) 
+    n <- length(stat)
+    nset <- sapply(sets, length)
+    rws <- 1:n
+    names(rws) <- names(stat)
+    sets <- lapply(sets, function(x) {rws[x]}) 
+    for (i in 1:nperm) {
+      rws <- sample(1:n, 1)
+      o <- c(rws:n, 1:(rws - 1))
+      pstat <- stat[o]
+      if (method == "mean") setTP <- sapply(sets, function(x) {mean(pstat[x])})
+      if (method == "sum") setTP <- sapply(sets, function(x) {sum(pstat[x])})
+      if (method == "max") setTP <- sapply(sets, function(x) {max(pstat[x])})
+      p <- p + as.numeric(setT > setTP) 
+    }  
+    p <- 1 - p / nperm
+    setT <- data.frame(setT, nset, p)
+  }
+     
+  return(setT)
+
+}
+
+msetTest <- function(stat = NULL, sets = NULL, nperm = NULL, method = "sum") {
+     
+     setT <- apply(stat, 2, function(x) {setTest(stat = x, sets = sets, nperm = nperm, method = method)})
+     names(setT) <- colnames(stat)
+     setT  
+
+} 
+
+####################################################################################################################
+#' 
+#' Genetic marker set tests based on the covariance test
+#'
+#' @description
+#' Genetic marker set tests based on the covariance statistics for a set of genetic markers.
+#' 
+#' @details
+#' The covariance test statistic is derived from a GBLUP (or GFBLUP) model fit. It is a measure of covariance between the total genomic effect for all markers 
+#' and the genomic effect for the genetic markers in the genomic feature. It also relates to the explained sums of
+#' squares for the genetic markers. 
+#' The distribution of this test statistic under the null hypothesis is difficult to describe in terms of exact or approximate 
+#' distributions, and an empirical distribution is required.
+#' 
+#' @param fit is the fit object obtained from a linear mixed model fit using the greml function
+#' @param g vector (or list) of genetic effects obtained from a linear mixed model fit (GBLUP of GFBLUP)
+#' @param s vector (or list) of single marker effects obtained from a linear mixed model fit (GBLUP of GFBLUP)
+#' @param W matrix of centered and scaled genotypes (n x m)
+#' @param sets list of marker sets corresponding to columns in W
+#' @param nperm number of permutations
+#' @return Returns a dataframe including 
+#' \item{setT}{covariance test statistics} 
+#' \item{nset}{number of markers in the set}
+#' \item{p}{p-value}
+#' @author Peter Sørensen
+#’ @references Rohde, P. D., Demontis, D., Cuyabano, B. C. D., Børglum, A. D., & Sørensen, P. (2016). Covariance Association Test (CVAT) Identifies Genetic Markers Associated with Schizophrenia in Functionally Associated Biological Processes. Genetics, 203(4), 1901-1913.
+#' @examples
+#' 
+#' # Simulate data
+#' W <- matrix(rnorm(20000000), ncol = 10000)
+#' 	colnames(W) <- as.character(1:ncol(W))
+#' 	rownames(W) <- as.character(1:nrow(W))
+#' y <- rowSums(W[, 1:10]) + rowSums(W[, 1001:1010]) + rnorm(nrow(W))
+#'
+#' # Create model
+#' data <- data.frame(y = y, mu = 1)
+#' fm <- y ~ 0 + mu
+#' X <- model.matrix(fm, data = data)
+#'
+#' # Create framework for lists
+#' setsGB <- list(A = colnames(W)) # gblup model
+#' setsGF <- list(C1 = colnames(W)[1:1000], C2 = colnames(W)[1001:2000], C3 = colnames(W)[2000:10000]) # gfblup model
+#' setsGT <- list(C1 = colnames(W)[1:10], C2 = colnames(W)[1001:1010], C3 = colnames(W)[1:10000]) # true model
+#'
+#' # Compute G
+#' G <- computeG(W = W)
+#' GB <- lapply(setsGB, function(x) {computeG(W = W[, x])})
+#' GF <- lapply(setsGF, function(x) {computeG(W = W[, x])})
+#' GT <- lapply(setsGT, function(x) {computeG(W = W[, x])})
+#'
+#' # REML analyses and multi marker association (set) test
+#' fitGB <- greml(y = y, X = X, G = GB, verbose = TRUE)
+#'
+#' # Use fit object as input
+#' cvat(fit = fitGB, W = W, sets = setsGF, nperm = 1000)
+#' cvat(fit = fitGB, W = W, sets = setsGT, nperm = 1000)
+#'
+#' # Use single coefficients as input 
+#' s <- crossprod(W / ncol(W), fitGB$Py) * fitGB$theta[1]
+#' cvat(s = s, W = W, sets = setsGF, nperm = 1000)
+#' cvat(s = s, W = W, sets = setsGT, nperm = 1000)
+#' 
+#' @export
+#'
+
+cvat <- function(fit = NULL, s = NULL, g = NULL, W = NULL, sets = NULL, nperm = 100) {
+     if (!is.null(fit)) {s <- crossprod(W/ncol(W),fit$Py)*fit$theta[1]}
+     Ws <- t(t(W) * as.vector(s))
+     if (is.null(g)) g <- W %*% s   
+     cvs <- colSums(as.vector(g) * Ws)
+     #setT <- setTest(stat = cvs, sets = sets, nperm = nperm, method = "sum")$p
+     #names(setT) <- names(sets)
+     setT <- setTest(stat = cvs, sets = sets, nperm = nperm, method = "sum")
+     if (!is.null(names(sets))) rownames(setT) <- names(sets)
+     return(setT)
+
+}
+
+scoreTest <- function(e = NULL, W = NULL, sets = NULL, nperm = 100) {
+     
+     we2 <- as.vector((t(W) %*% e)**2)   
+     names(we2) <- colnames(W)       
+     setT <- setTest(stat = we2, sets = sets, nperm = nperm, method = "sum")$p
+     return(setT)
+
+}
+
+
+
+####################################################################################################################
+#' 
+#' Genetic marker set tests based on the hyperG test
+#' 
+#' @description
+#' Genetic marker set tests based on the hyperG test statistics for a set of genetic markers.
+#'
+#' @details
+#' The hyperG marker set test tests a predefined set of markers (i.e. those within a particular genomic feature)
+#' for an association with the trait phenotype.
+#' Under the null hypothesis (associated markers are picked at random from the total number of tested 
+#' genetic markers) it is assumed that the observed count statistic is a realization from a hypergeometric 
+#' distribution.
+#' This hypothesis can be formulated and tested in a number of ways. Here we consider a test statistic based 
+#' on counting the number of genetic markers in the feature that are associated to trait phenotype. 
+#' A test based on the count test statistic is likely to have high power to detect association if the genomic 
+#' feature harbours genetic markers with large effects. 
+#' 
+#' @param p vector of single marker p-values (e.g. based on a t-stat)
+#' @param sets list of marker sets - names corresponds to rownames in stat
+#' @param threshold single marker p-value cut-off
+#' @return Returns vector of p values with length equal to the number of sets 
+#' @author Peter Sørensen
+#’ @references Rohde, P. D., Edwards S. M., Sarup P., Sørensen, P. (August, 2014). Gene-based Association Approach Identify Genes Across Stress Traits in Fruit Flies. Poster presented at the 10th World Congress of Genetics Applied to Livestock Production (WCGALP), Vancouver, Canada.
+#' @examples
+#' 
+#' # Simulate data
+#' W <- matrix(rnorm(2000000), ncol = 10000)
+#'   colnames(W) <- as.character(1:ncol(W))
+#' y <- rowSums(W[, 1:10]) + rowSums(W[, 1001:1010]) + rnorm(nrow(W))
+#' 
+#' # REML analyses 
+#' data <- data.frame(y = y, mu = 1)
+#' fm <- y ~ mu
+#' fit <- gfm(fm = fm, W = W, sets = list(colnames(W)), data = data)
+#' 
+#' # hyperG set test 
+#' sets <- list(A = as.character(1:100), B = as.character(101:1000), C = as.character(1001:5000), D = as.character(5001:10000))
+#' p <- pt(fit$s / fit$vs)
+#' res <- hgTest(p = p, sets = sets, threshold = 0.05)
+#' 
+#' @export
+#'
+
+hgTest <- function(p = NULL, sets = NULL, threshold = 0.05) {
+     
+     N <- length(p)
+     Na <- sum(p < threshold)
+     Nna <- N - Na
+     Nf <- sapply(sets, length)
+     Naf <- sapply(sets, function(x) {sum(p[x] < threshold)})
+     Nnaf <- Nf - Naf
+     Nanf <- Na - Naf
+     Nnanf <- Nna - Nnaf
+     phyperg <- 1 - phyper(Naf - 1, Nf, N - Nf, Na)
+     phyperg
+
+}
+
+
---
+++ R/single_marker_test.R
@@ -0,0 +1,127 @@
+####################################################################################################################
+#    Module 5: LMM marker association test
+####################################################################################################################
+#'
+#' Linear mixed model marker association test
+#'
+#' @description
+#' Marker test using linear mixed model (LMM) to test for an association of single markers with a phenotype.
+#'
+#' @details
+#' Linear mixed model single marker association (LMMA) statistics are based on either exact or approximate methods.
+#' Exact methods estimate variance components and effects of single markers jointly.
+#' Approximate methods estimate single marker effects conditionally.
+#'
+#' @param fit list of information about linear model fit (output from greml)
+#' @param W matrix of centered and scaled genotypes (n x m)
+#' @param m is the total number of markers in W 
+#' @param statistic is the single marker test statistic used. Default is the "mastor", alternatives include "gblup", "bolt-lmm" and "grammar-gamma"
+#' @return Returns a dataframe including 
+#' \item{coef}{single marker coefficients} 
+#' \item{se}{standard error of coefficients}
+#' \item{stat}{single marker test statistic}
+#' \item{p}{p-value}
+#' @author Peter Sørensen
+#' @references Chen, W. M., & Abecasis, G. R. (2007). Family-based association tests for genomewide association scans. The American Journal of Human Genetics, 81(5), 913-926.
+#' @references Loh, P. R., Tucker, G., Bulik-Sullivan, B. K., Vilhjalmsson, B. J., Finucane, H. K., Salem, R. M., ... & Patterson, N. (2015). Efficient Bayesian mixed-model analysis increases association power in large cohorts. Nature genetics, 47(3), 284-290.
+#' @references Kang, H. M., Sul, J. H., Zaitlen, N. A., Kong, S. Y., Freimer, N. B., Sabatti, C., & Eskin, E. (2010). Variance component model to account for sample structure in genome-wide association studies. Nature genetics, 42(4), 348-354.
+#' @references Lippert, C., Listgarten, J., Liu, Y., Kadie, C. M., Davidson, R. I., & Heckerman, D. (2011). FaST linear mixed models for genome-wide association studies. Nature methods, 8(10), 833-835.
+#' @references Listgarten, J., Lippert, C., Kadie, C. M., Davidson, R. I., Eskin, E., & Heckerman, D. (2012). Improved linear mixed models for genome-wide association studies. Nature methods, 9(6), 525-526.
+#' @references Listgarten, J., Lippert, C., & Heckerman, D. (2013). FaST-LMM-Select for addressing confounding from spatial structure and rare variants. Nature Genetics, 45(5), 470-471.
+#' @references Lippert, C., Quon, G., Kang, E. Y., Kadie, C. M., Listgarten, J., & Heckerman, D. (2013). The benefits of selecting phenotype-specific variants for applications of mixed models in genomics. Scientific reports, 3.
+#' @references Zhou, X., & Stephens, M. (2012). Genome-wide efficient mixed-model analysis for association studies. Nature genetics, 44(7), 821-824.
+#' @references Svishcheva, G. R., Axenovich, T. I., Belonogova, N. M., van Duijn, C. M., & Aulchenko, Y. S. (2012). Rapid variance components-based method for whole-genome association analysis. Nature genetics, 44(10), 1166-1170.
+#' @references Yang, J., Zaitlen, N. A., Goddard, M. E., Visscher, P. M., & Price, A. L. (2014). Advantages and pitfalls in the application of mixed-model association methods. Nature genetics, 46(2), 100-106.
+#' @references Bulik-Sullivan, B. K., Loh, P. R., Finucane, H. K., Ripke, S., Yang, J., Patterson, N., ... & Schizophrenia Working Group of the Psychiatric Genomics Consortium. (2015). LD Score regression distinguishes confounding from polygenicity in genome-wide association studies. Nature genetics, 47(3), 291-295.
+#' @examples
+#'
+#' # Simulate data
+#' W <- matrix(rnorm(20000000), ncol = 10000)
+#' 	colnames(W) <- as.character(1:ncol(W))
+#' 	rownames(W) <- as.character(1:nrow(W))
+#' y <- rowSums(W[, 1:10]) + rowSums(W[, 1001:1010]) + rnorm(nrow(W))
+#'
+#' # Create model
+#' data <- data.frame(y = y, mu = 1)
+#' fm <- y ~ 0 + mu
+#' X <- model.matrix(fm, data = data)
+#'
+#' # Create framework for lists
+#' setsGB <- list(A = colnames(W)) # gblup model
+#' setsGF <- list(C1 = colnames(W)[1:1000], C2 = colnames(W)[1001:2000], C3 = colnames(W)[2000:10000]) # gfblup model
+#' setsGT <- list(C1 = colnames(W)[1:10], C2 = colnames(W)[1001:1010], C3 = colnames(W)[1:10000]) # true model
+#'
+#' # Compute G
+#' G <- computeG(W = W)
+#' GB <- lapply(setsGB, function(x) {computeG(W = W[, x])})
+#' GF <- lapply(setsGF, function(x) {computeG(W = W[, x])})
+#' GT <- lapply(setsGT, function(x) {computeG(W = W[, x])})
+#'
+#' # REML analyses and single marker association test
+#' fitGB <- greml(y = y, X = X, G = GB, verbose = TRUE)
+#' maGB <- lmma(fit = fitGB, W = W)
+#'
+#' @export
+#'
+
+lmma <- function( fit=NULL, W=NULL, m=NULL, statistic="mastor") {
+  
+  if (is.null(m)) m <- ncol(W)
+  n <- nrow(W)
+  # get linear model fit results
+  Sg <- fit$theta[1]
+  Py <- fit$Py
+  Vy <- fit$Vy
+  yVy <- fit$yVy
+  trPG <- fit$trPG[1]
+  trVG <- fit$trVG[1]
+  
+  # compute single marker, coefficients, test statistics and p-values
+  nW <- colSums(!W==0)
+  WPy <- crossprod(W,Py)
+  WVy <- crossprod(W,Vy)
+  ww <- colSums(W**2)
+  
+  if (statistic=="mastor") {
+    coef <- (WPy/(trPG/m))/m
+    se <- (1/(trPG/m))/m
+    stat <- WPy**2/sum(Py**2)
+    p <- pchisq(stat,df=1,ncp=0, lower.tail=FALSE)
+  }
+  
+  mma <- data.frame(coef=coef,se=se,stat=stat,p=p)
+  rownames(mma) <- colnames(W)
+  
+  return(mma)
+  
+}
+
+#' @export
+
+plotma <- function(ma=NULL,chr=NULL,rsids=NULL,thresh=5) {
+  
+  mlogObs <- -log10(ma$p)
+  m <- length(mlogObs)
+  rwsSNP <- rsids
+  if(!is.null(thresh)) { rwsSNP <- mlogObs > thresh }  
+  if(is.null(chr)) { chr <- rep(1,m) }  
+  
+  layout(matrix(1:2,ncol=1))
+  o <- order(mlogObs,decreasing=TRUE)
+  mlogExp <- -log10( (1:m)/m ) 
+  plot(y=mlogObs[o],x=mlogExp, col=2, pch="+",
+       frame.plot=FALSE, main="", xlab="Expected -log10(p)", ylab="Observed -log10(p)")
+  abline(a=0,b=1)
+  
+  colSNP <- "red"
+  
+  is.even <- function(x) x %% 2 == 0
+  colCHR <- rep(gray(0.3),m)
+  colCHR[is.even(chr)] <- gray(0.9)
+  
+  plot(y=mlogObs,x=1:m,ylab="Observed -log10(p)",xlab="Position",col=colCHR,   
+       pch=".",frame.plot=FALSE)
+  points(y=mlogObs[rwsSNP],x=(1:m)[rwsSNP],col=colSNP)  
+  abline(h=thresh,col=2,lty=2)
+}   
+
