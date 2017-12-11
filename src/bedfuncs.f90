8a60f216d27aaa0cf18d144eb6d3079d6ac7bd6f
psoerensen <pso@mbg.au.dk>
2017-12-11 13:32:26 +0100

    added bedsolve

--- src/bedfuncs.f90
+++ src/bedfuncs.f90
@@ -29,6 +29,31 @@
 
     end function raw2int
 
+    function raw2real(n,nbytes,raw) result(w)
+    implicit none
+
+    integer, intent(in) :: nbytes,n
+    integer, parameter :: byte = selected_int_kind(1) 
+    integer(byte), intent(in) :: raw(nbytes)
+    integer*4 :: i,j,k,rawbits
+    real*8 :: w(n)
+    real*8, dimension(4) :: rawcodes
+ 
+    rawcodes = (/ 0.0D0, 3.0D0, 1.0D0, 2.0D0 /)
+    
+    w=0.0D0
+    k=0
+    do i=1,nbytes 
+      do j=0,6,2
+        k = k + 1
+        rawbits = ibits(raw(i), j, 2)
+        w(k) = rawcodes(rawbits+1)
+        if (k==n) exit 
+      enddo
+    enddo
+
+    end function raw2real
+
 
     function scale(nr,g) result(w)
     implicit none
@@ -209,3 +234,77 @@
   close (unit=13)
 
   end subroutine prsbed
+
+
+
+!==============================================================================================================
+  subroutine bedsolve(n,nr,rws,nc,cls,scaled,nbytes,fnRAW,nit,lambda,tol,y,g,e,s,mean,sd)
+!==============================================================================================================
+
+  use bedfuncs 
+
+  implicit none
+  
+  integer*4 :: i,j,k,n,nr,nc,rws(nr),cls(nc),scaled,nbytes,nit,it
+  real*8 :: y(n),e(n),raww(n),w(n),g(n)
+  real*8 :: dww(nc),s(nc),os(nc),lambda(nc),mean(nc),sd(nc)
+  real*8 :: lhs,rhs,snew,tol,sigma
+  character(len=1000) :: fnRAW
+
+  integer, parameter :: byte = selected_int_kind(1) 
+  integer(byte) :: raw(nbytes)
+  integer :: stat
+
+  open(unit=13, file=fnRAW, status='old', access='direct', form='unformatted', recl=nbytes)
+
+  ! genotypes coded 0,1,2,3=missing => where 0,1,2 means 0,1,2 copies of alternative allele 
+  do i=1,nc
+  read(13, iostat=stat, rec=cls(i)) raw
+  if (stat /= 0) exit
+  raww = raw2real(n,nbytes,raw)
+  where (raww.ne.0.0D0)
+  w = (raww-mean(i))/sd(i)
+  elsewhere
+  w = 0.0D0
+  end where
+  dww(i)=dot_product(w(rws),w(rws)) 
+  if(s(i).eq.0.0D0) then 
+    s(i)=(dot_product(w(rws),y(rws))/dww(i))/nc
+  endif     
+  enddo
+  close (unit=13)
+  os=s
+
+  e=0.0D0
+  e(rws)=y(rws)
+
+  do it=1,nit
+  g=0.0D0
+  open (unit=13,file=fnRAW, status='old', form='unformatted', access='direct', recl=n)
+  do i=1,nc
+  read(13, iostat=stat, rec=cls(i)) raw
+  if (stat /= 0) exit
+  raww = raw2real(n,nbytes,raw)
+  where (raww.ne.0.0D0)
+  w = (raww-mean(i))/sd(i)
+  elsewhere
+  w = 0.0D0
+  end where
+  lhs=dww(i)+lambda(i)
+  rhs=dot_product(w(rws),e(rws)) + dww(i)*s(i)
+  snew=rhs/lhs
+  
+  e(rws)=e(rws) - w(rws)*(snew-s(i))
+  s(i)=snew
+  g=g+w*s(i)
+  enddo
+  close (unit=13)
+  print*,(sum((s-os)**2)/sqrt(dble(nc)))
+  if( (sum((s-os)**2)/sqrt(dble(nc)))<tol) exit  
+  os=s  
+  enddo
+  
+  nit=it
+  tol=sum((s-os)**2)
+  
+  end subroutine bedsolve
