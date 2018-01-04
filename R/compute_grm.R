c6c289b9bda0f94915c994dcc7109d1aa807445f
psoerensen <pso@mbg.au.dk>
2018-01-04 21:15:31 +0100

    updated computeG

--- R/compute_grm.R
+++ R/compute_grm.R
@@ -81,7 +81,9 @@
 #'
 
 
-computeG <- function(Wlist=NULL,ids=NULL,rsids=NULL,rws=NULL,cls=NULL,scaled=TRUE, msize=100, ncores=1) { 
+computeG <- function(Wlist=NULL,ids=NULL,rsids=NULL,rws=NULL,cls=NULL,scaled=TRUE, msize=100, ncores=1, fnG=NULL, updateG=FALSE) {
+
+ 
      n <- Wlist$n
      m <- Wlist$m
      nbytes <- ceiling(n/4)
@@ -94,6 +96,25 @@ computeG <- function(Wlist=NULL,ids=NULL,rsids=NULL,rws=NULL,cls=NULL,scaled=TRU
      }
      nr <- length(rws)
      fnRAW <- Wlist$fnRAW
+
+     # Initiate Glist
+     idsG <- Wlist$ids[rws]
+     rsidsG <- Wlist$rsids[cls] 
+     nG <- length(idsG)
+     mG <- length(rsidsG) 
+     Glist <- list(fnG=fnG,ids=idsG,rsids=rsidsG,n=nG,m=mG)
+     
+     # Initiate G file
+     if(!is.null(fnG)) {
+          if (!updateG)  {
+               if(file.exists(fnG)) stop("G file name allready exist")
+               bfG <- file(fnG,"wb")
+               seek(bfG,8*(nG**2))
+               writeBin(raw(8),bfG)
+               close(bfG)
+          }
+     }
+
      res <- .Fortran("grmbed", 
                      n = as.integer(n),
                      nr = as.integer(nr),
@@ -109,5 +130,26 @@ computeG <- function(Wlist=NULL,ids=NULL,rsids=NULL,rws=NULL,cls=NULL,scaled=TRU
                      PACKAGE = 'qgg'
                      
      )
-     return(res$G)
+     rownames(res$G) <- colnames(res$G) <- idsG
+          if(!is.null(fnG)) {
+               paste("Writing G to file")
+               idsG1 <- rownames(G)
+               idsG2 <- colnames(G)
+               n1 <- length(idsG1)     
+               n2 <- length(idsG2)
+               indx1 <- match(idsG1,Glist$idsG)		# no reorder needed
+               indx2 <- match(idsG2,Glist$idsG)		# no reorder needed
+               bfG <- file(Glist$fnG,"r+b")
+               for (j in 1:n1) {
+                    k <- indx1[j]
+                    where <- (k-1)*Glist$n + indx2[1]-1
+                    seek(bfG,where=where*8, rw="write")
+                    writeBin( G[j,1:n2]/m, bfG, size = 8, endian = "little")
+                    gc()
+               }
+               close(bfG)
+          }
+
+     if(is.null(fnG)) return(res$G)
+     if(!is.null(fnG)) return(Glist)
 }
