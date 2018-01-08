037ad1b04184fcdd622b6f9499b8c14fe9f63067
psoerensen <pso@mbg.au.dk>
2018-01-08 22:58:14 +0100

    solvebed

--- src/bedfuncs.f90
+++ src/bedfuncs.f90
@@ -293,8 +293,8 @@
   dww(i)=dot_product(w(rws),w(rws))
   if(s(i).eq.0.0D0) then
     s(i)=0.0D0  
-    !s(i)=(dot_product(w(rws),y(rws))/dww(i))/nc
-    s(i)=(ddot(nr,w(rws),1,y(rws),1)/dww(i))/nc
+    s(i)=(dot_product(w(rws),y(rws))/dww(i))/nc
+    !s(i)=(ddot(nr,w(rws),1,y(rws),1)/dww(i))/nc
   endif     
   enddo
   close (unit=13)
@@ -316,8 +316,8 @@
   w = 0.0D0
   end where
   lhs=dww(i)+lambda(i)
-  rhs=ddot(nr,w(rws),1,e(rws),1) + dww(i)*s(i)
-  !rhs=dot_product(w(rws),e(rws)) + dww(i)*s(i)
+  !rhs=ddot(nr,w(rws),1,e(rws),1) + dww(i)*s(i)
+  rhs=dot_product(w(rws),e(rws)) + dww(i)*s(i)
   snew=rhs/lhs
   
   e(rws)=e(rws) - w(rws)*(snew-s(i))
