b49f200adc920379ba8e5ac677eb30101e7e7ff1
psoerensen <pso@mbg.au.dk>
2018-07-16 20:25:55 +0200

    readbin

--- src/bedfuncs.f90
+++ src/bedfuncs.f90
@@ -902,25 +902,25 @@
   integer (kind=k14) :: pos(nc),nbytes14,offset14,i14
 
 
-  !nchar=index(fnBIN, '.bin')
-  !open(unit=13, file=fnBIN(1:(nchar+3)), status='old', access='stream', form='unformatted', action='read')
-  !nbytes14 = nbytes
-  !offset14 = 0
-  !do i=1,nc 
-  !  i14=cls(i)
-  !  pos(i) = 1 + offset14 + (i14-1)*nbytes14
-  !  read(13, pos=pos(i)) raw
-  !  W(1:nr,i) = raw(rws)
-  !  call flush(13)
-  !enddo
-
   nchar=index(fnBIN, '.bin')
-  open(unit=13, file=fnBIN(1:(nchar+3)), status='old', access='direct', form='unformatted', recl=n*8)
+  open(unit=13, file=fnBIN(1:(nchar+3)), status='old', access='stream', form='unformatted', action='read')
+  nbytes14 = nbytes*n
+  offset14 = 0
   do i=1,nc 
-    read(13, rec=cls(i)) raw
+    i14=cls(i)
+    pos(i) = 1 + offset14 + (i14-1)*nbytes14
+    read(13, pos=pos(i)) raw
     W(1:nr,i) = raw(rws)
     call flush(13)
   enddo
+
+  !nchar=index(fnBIN, '.bin')
+  !open(unit=13, file=fnBIN(1:(nchar+3)), status='old', access='direct', form='unformatted', recl=n*8)
+  !do i=1,nc 
+  !  read(13, rec=cls(i)) raw
+  !  W(1:nr,i) = raw(rws)
+  !  call flush(13)
+  !enddo
 
   close(unit=13)
 
