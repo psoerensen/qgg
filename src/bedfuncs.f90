9225e83cf576fcfe6737138e974324336c66ae85
psoerensen <pso@mbg.au.dk>
2018-07-21 14:36:21 +0200

    mmap

--- src/bedfuncs.f90
+++ src/bedfuncs.f90
@@ -1036,7 +1036,7 @@
     end interface 
 
     interface
-    integer(c_int) function munmap(addr, len) bind(c,name='munmap')
+    type(c_ptr) function munmap(addr, len) bind(c,name='munmap')
     use iso_c_binding  
     integer(c_int), value :: addr 
     integer(c_size_t), value :: len
