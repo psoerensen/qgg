9ccc8ddbff0b472ff1300cd3d5928c139527c9c8
psoerensen <pso@mbg.au.dk>
2018-07-21 17:35:22 +0200

    mmap

--- src/bedfuncs.f90
+++ src/bedfuncs.f90
@@ -1081,7 +1081,8 @@
 
     do i = 1,nc
     off = 0 + (i-1)*n*nbytes
-    adr = mmap(loc(null),len,prot_read,map_private,fd,off)
+    !adr = mmap(loc(null),len,prot_read,map_private,fd,off)
+    adr = mmap(loc(null),len,prot_read,1,fd,off)
     call memcpy(loc(mapx), adr, len)
     W(1:nr,i)=mapx(1:n) 
     k = munmap(adr, len)
