38a74735f26f677e73c2970fcdbc89f21e909ab9
psoerensen <pso@mbg.au.dk>
2018-07-21 15:14:22 +0200

    mmap

--- src/bedfuncs.f90
+++ src/bedfuncs.f90
@@ -1024,9 +1024,9 @@
 
     !use iso_c_binding 
     interface 
-    type(c_intptr_t) function mmap(addr,len,prot,flags,fildes,off) bind(c,name='mmap') 
+    type(c_ptr) function mmap(addr,len,prot,flags,fildes,off) bind(c,name='mmap') 
     use iso_c_binding 
-    integer(c_intptr_t), value :: addr 
+    integer(c_int), value :: addr 
     integer(c_size_t), value :: len 
     integer(c_int), value :: prot 
     integer(c_int), value :: flags 
