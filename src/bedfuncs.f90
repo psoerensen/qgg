87994ab87ab970866947f8cebbe0cb8026d3deee
psoerensen <pso@mbg.au.dk>
2018-07-23 00:17:51 +0200

    mmap

--- src/bedfuncs.f90
+++ src/bedfuncs.f90
@@ -1102,6 +1102,7 @@
 
     
     !off = 0 !+ (cls(i)-1)*n*nbytes
+    print*,len,off
     cptr = mmap(0,len,prot_read,map_share,fd,off) 
     !cptr = mmap(0,len,prot_read,map_private,fd,off) 
     call c_f_pointer(cptr,x,[len]) 
