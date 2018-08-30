e8e40541a0fe702bb561c069e835dd62db6fa2d8
psoerensen <pso@mbg.au.dk>
2018-08-30 12:19:24 +0200

    computeGRM

--- R/compute_grm.R
+++ R/compute_grm.R
@@ -14,8 +14,8 @@ computeGRM <- function(Glist=NULL,ids=NULL,rsids=NULL,rws=NULL,cls=NULL, W=NULL,
 
      if (!is.null(W)) { 
           SS <- tcrossprod(W)                              # compute crossproduct, all SNPs
-          N <- tcrossprod(!W == miss)                      # compute number of observations, all SNPs
-          G <- SS / N
+          #N <- tcrossprod(!W == miss)                      # compute number of observations, all SNPs
+          G <- SS / ncol(W)
           return(G)
      }     
      if (is.null(W)) { 
