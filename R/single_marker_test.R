5a742b789964b93ee5a1391d4b28eb1ca322e0e5
psoerensen <pso@mbg.au.dk>
2018-10-10 22:39:56 +0200

    Update R/single_marker_test.R

--- R/single_marker_test.R
+++ R/single_marker_test.R
@@ -166,7 +166,7 @@ lma <- function( y=NULL, X=NULL, W=NULL, Glist=NULL, ids=NULL, rsids=NULL, msize
                print(paste("Finished block",i,"out of",nsets,"blocks"))
           }
           cls <- unlist(sets)
-          res <- list(s=s[cls,],se=se[cls,],stat=stat[cls,],p=p[cls,])
+          res <- data.frame(s=s[cls,],se=se[cls,],stat=stat[cls,],p=p[cls,])
      }
      return(res)
 }
