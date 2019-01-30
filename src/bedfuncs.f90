a43abf3bc400cc659174da896d9c8a039d800356
psoerensen <pso@mbg.au.dk>
2019-01-30 00:23:51 +0000

    Update bedfuncs.f90

--- src/bedfuncs.f90
+++ src/bedfuncs.f90
@@ -254,9 +254,9 @@
   nbytes14 = nbytes
   offset14 = offset
 
-  !open(unit=13, file=fnRAW(1:(nchar+3)), status='old', access='stream', form='unformatted', action='read')
+  open(unit=13, file=fnRAW(1:(nchar+3)), status='old', access='stream', form='unformatted', action='read')
   !open(unit=13, file=fnRAW(1:(nchar+3)), status='old', access='direct', form='unformatted', recl=nbytes)
-  open(unit=13, file=fnRAW(1:(nchar+3)), status='old', access='sequential', form='unformatted')
+  !open(unit=13, file=fnRAW(1:(nchar+3)), status='old', access='sequential', form='unformatted')
   
   ntotal=dble(nr)  
 
