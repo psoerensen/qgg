6a2213811ddf89d331600354d9e8d4550a1c0fde
psoerensen <pso@mbg.au.dk>
2019-02-12 10:20:53 +0000

    Update bedfuncs.f90

--- src/bedfuncs.f90
+++ src/bedfuncs.f90
@@ -290,8 +290,8 @@
       where(gsc==3.0D0) gsc=2.0D0*af(i)
       if ( nmiss==ntotal ) gsc=0.0D0
       do j=1,nprs
-        !if (s(i,j)/=0.0d0) prsmp(1:nr,j,thread) = prsmp(1:nr,j,thread) + gsc*s(i,j)
-        prsmp(1:nr,j,thread) = prsmp(1:nr,j,thread) + gsc*s(i,j)
+        if (s(i,j)/=0.0d0) prsmp(1:nr,j,thread) = prsmp(1:nr,j,thread) + gsc*s(i,j)
+        !prsmp(1:nr,j,thread) = prsmp(1:nr,j,thread) + gsc*s(i,j)
       enddo  
     endif
   enddo 
