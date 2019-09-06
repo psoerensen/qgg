b846d19a6c224fd76ce159e13091539ec33715ec
psoerensen <pso@mbg.au.dk>
2019-09-06 16:53:09 +0100

    Update genomic_reml.R

--- R/genomic_reml.R
+++ R/genomic_reml.R
@@ -329,8 +329,6 @@ remlf <- function(y = NULL, X = NULL, GRMlist = NULL, G = NULL, theta = NULL, id
 
   fnr <- paste(paste(sample(letters, 10, replace = TRUE), collapse = ""), ".qgg", sep = "")
   write.table(as.character(rfnames), file = "param.qgg", quote = FALSE, sep = " ", col.names = FALSE, row.names = FALSE)
-  file.remove("param.qgg")
-  
 
   fit <- .Fortran("reml",
     n = as.integer(n),
@@ -357,7 +355,8 @@ remlf <- function(y = NULL, X = NULL, GRMlist = NULL, G = NULL, theta = NULL, id
     trVG = as.double(rep(0, nr)),
     PACKAGE = "qgg"
   )
-
+  file.remove("param.qgg")
+  
   fit$ids <- names(y)
   fit$yVy <- sum(y * fit$Vy)
   fit$wd <- getwd()
