aad68462376994e1e3f9c4f12a65e1c4dd767881
Peter Sørensen <au223366@uni.au.dk>
2021-06-08 10:01:21 +0200

    Update RcppExports.R

--- R/RcppExports.R
+++ R/RcppExports.R
@@ -21,16 +21,16 @@ summarybed <- function(file, n, cls, af, y) {
     .Call(`_qgg_summarybed`, file, n, cls, af, y)
 }
 
-bayes <- function(y, W, b, lambda, pi, vara, varb, vare, nub, nue, updateB, updateE, updatePi, nit, method) {
-    .Call(`_qgg_bayes`, y, W, b, lambda, pi, vara, varb, vare, nub, nue, updateB, updateE, updatePi, nit, method)
+bayes <- function(y, W, b, lambda, pi, vara, varb, vare, ssb_prior, sse_prior, nub, nue, updateB, updateE, updatePi, nit, method) {
+    .Call(`_qgg_bayes`, y, W, b, lambda, pi, vara, varb, vare, ssb_prior, sse_prior, nub, nue, updateB, updateE, updatePi, nit, method)
 }
 
 fbayes <- function(y, W, LD, b, lambda, pi, vara, varb, vare, nub, nue, updateB, updateE, updatePi, nit, method) {
     .Call(`_qgg_fbayes`, y, W, LD, b, lambda, pi, vara, varb, vare, nub, nue, updateB, updateE, updatePi, nit, method)
 }
 
-sbayes <- function(wy, LD, b, lambda, pi, vara, varb, vare, nub, nue, updateB, updateE, updatePi, n, nit, method) {
-    .Call(`_qgg_sbayes`, wy, LD, b, lambda, pi, vara, varb, vare, nub, nue, updateB, updateE, updatePi, n, nit, method)
+sbayes <- function(wy, LD, b, lambda, yy, pi, vara, varb, vare, ssb_prior, sse_prior, nub, nue, updateB, updateE, updatePi, n, nit, method) {
+    .Call(`_qgg_sbayes`, wy, LD, b, lambda, yy, pi, vara, varb, vare, ssb_prior, sse_prior, nub, nue, updateB, updateE, updatePi, n, nit, method)
 }
 
 grsbed <- function(file, n, cls, af, b) {
