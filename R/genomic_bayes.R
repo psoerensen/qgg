bec514ca0fdaea0fbcf040b7b56e422432cae932
Peter Sørensen <au223366@uni.au.dk>
2021-07-08 12:59:31 +0200

    Update genomic_bayes.R

--- R/genomic_bayes.R
+++ R/genomic_bayes.R
@@ -140,9 +140,12 @@ gbayes <- function(y=NULL, X=NULL, W=NULL, Glist=NULL, chr=NULL, rsids=NULL, b=N
            pim[[i]] <- fitset$Pi
            print(paste("Finished segment:",i,"out of",length(sets),"segments on chromosome:",chr))
          }
-         fit[[chr]] <- list(bm=bm,dm=dm,E=varem,B=varbm,Pi=pim, e=e, g=y-e)
+         #fit[[chr]] <- list(bm=bm,dm=dm,E=varem,B=varbm,Pi=pim, e=e, g=y-e)
+         fit[[chr]] <- list(bm=bm,dm=dm,E=varem,B=varbm,Pi=pim)
        }
-       
+       fit$g <- g
+       fit$e <- e
+       fit$acc <- acc(yobs=y,ypred=g)
      }
      
           
