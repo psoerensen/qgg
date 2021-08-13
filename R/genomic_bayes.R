7914cb7456ab3a8a07c551f10806f39eeabb3a25
Peter Sørensen <au223366@uni.au.dk>
2021-08-13 09:11:01 +0200

    Update genomic_bayes.R

--- R/genomic_bayes.R
+++ R/genomic_bayes.R
@@ -161,7 +161,7 @@ gbayes <- function(y=NULL, X=NULL, W=NULL, Glist=NULL, chr=NULL, rsids=NULL, b=N
        if(is.null(chr)) chromosomes <- 1:Glist$nchr
        if(!is.null(chr)) chromosomes <- chr
        
-       bm <- dm <- covs <- vector(length=1:Glist$nchr,mode="list")
+       bm <- dm <- covs <- vector(length=Glist$nchr,mode="list")
        names(covs) <- names(bm) <- names(dm) <- 1:Glist$nchr
        
        e <- y
