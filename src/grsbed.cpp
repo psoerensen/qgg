299a4b361821e43c831c76be1592d02b25e1668e
Peter Sørensen <au223366@uni.au.dk>
2021-08-13 21:41:25 +0200

    Update grsbed.cpp

--- src/grsbed.cpp
+++ src/grsbed.cpp
@@ -118,7 +118,7 @@ std::vector<std::vector<float>> mtgrsbed( const char* file,
       map[0] = 2.0;
       map[1] = - 2.0*af[i];
       map[2] = 1.0 ;
-      map[3] = -2.0*af[i]/sqrtf(2.0*af[i]*(1.0-af[i]));
+      map[3] = 0.0;
     }
     for (size_t k = 0; k < nbytes; k++) {
       buf_k = buffer[k];
