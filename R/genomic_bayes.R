bf66b624e5c6ad43e43d3bcc22f951235ac54b2c
Peter Sørensen <au223366@uni.au.dk>
2021-10-07 13:19:53 +0200

    Update genomic_bayes.R

--- R/genomic_bayes.R
+++ R/genomic_bayes.R
@@ -772,39 +772,6 @@ plotCvs <- function(fit=NULL, causal=NULL) {
 }
 
 
-gsim <- function(nt=1,W=NULL,n=1000,m=1000) {
-  if(is.null(W)) {
-    W <- matrix(runif(n),ncol=1)
-    for (i in 2:m) {
-      W <- cbind(W,scale(W[,i-1]) + runif(n))  
-    }
-  }
-  n <- nrow(W)
-  m <- ncol(W)
-  if(is.null(colnames(W))) colnames(W) <- paste0("m",1:m)
-  if(is.null(rownames(W))) rownames(W) <- paste0("id",1:n)
-  
-  y <- e <- vector(length=nt,mode="list")
-  names(y) <- paste0("D",1:nt)
-  set0 <- sample(1:ncol(W),2)
-  set1 <- b1 <- g1 <- vector(length=nt,mode="list")
-  g <- NULL
-  for (i in 1:nt){
-    b0 <- sample(c(0.25,-0.25,0.5,-0.5),2)
-    g0 <- W[,set0]%*%b0
-    set1[[i]] <- sample(1:ncol(W),2)
-    b1[[i]] <- sample(c(0.25,-0.25,0.5,-0.5),length(set1[[i]]))
-    g1[[i]] <- W[,set1[[i]]]%*%b1[[i]]
-    e[[i]] <- rnorm(nrow(W),mean=0,sd=1)
-    y[[i]] <- as.vector(g0+g1[[i]]+e[[i]])
-    names(y[[i]]) <- rownames(W)
-    g <- cbind(g,g0+g1[[i]])
-  }
-  colnames(g) <- paste0("D",1:nt) 
-  if(nt==1) return( list( y=y[[1]],W=W, e=e[[1]],g=g,b0=b0,b1=b1,set0=set0,set1=set1,causal=c(set0,unlist(set1))))
-  if(nt>1) return( list( y=y,W=W, e=e,g=g,b0=b0,b1=b1,set0=set0,set1=set1,causal=c(set0,unlist(set1))))
-}
-
 
 splitWithOverlap <- function(vec, seg.length, overlap) {
      starts = seq(1, length(vec), by=seg.length-overlap)
