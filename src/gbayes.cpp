921b85e17c0b7bc8291bb6e1523d65502a55a5a8
Peter Sørensen <au223366@uni.au.dk>
2022-04-29 14:26:23 +0200

    Update gbayes.cpp

--- src/gbayes.cpp
+++ src/gbayes.cpp
@@ -589,7 +589,7 @@ std::vector<std::vector<double>>  sbayes_spa( std::vector<double> wy,
   int m = b.size();
   
   double rhs, lhs, bn, conv, diff;
-  double rhs0, rhs1, lhs0, lhs1, like0, like1, p0, v0, v1;
+  double rhs0, rhs1, lhs0, lhs1, like0, like1, p0, v0, v1, ri;
   double ssb, sse, ssg, dfb, dfe, chi2;
   double xtau, tau, lambda_tau, mu_tau, z, z2, u;
   
