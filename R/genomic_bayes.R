c1a810d2f375a9188e30ff7c8c18a9e4498fc5b0
Peter Sørensen <au223366@uni.au.dk>
2022-05-23 20:04:59 +0200

    Update genomic_bayes.R

--- R/genomic_bayes.R
+++ R/genomic_bayes.R
@@ -359,8 +359,8 @@ gbayes <- function(y=NULL, X=NULL, W=NULL, stat=NULL, covs=NULL, trait=NULL, fit
       
       b2 <- stat$b^2
       seb2 <- stat$seb^2
-      yy <- (b2 + (n-2)*seb2)*ww[rownames(stat),]
-      #yy <- (b2 + (n-2)*seb2)*stat$ww
+      #yy <- (b2 + (n-2)*seb2)*ww[rownames(stat),]
+      yy <- (b2 + (n-2)*seb2)*stat$ww
       yy <- mean(yy)
     }
     
@@ -391,8 +391,8 @@ gbayes <- function(y=NULL, X=NULL, W=NULL, stat=NULL, covs=NULL, trait=NULL, fit
       for (i in 1:nt) {
         seb2[,i] <- (n[i]-2)*seb2[,i]
       }
-      yy <- (b2 + (n-2)*seb2)*ww[rownames(stat),]
-      #yy <- (b2 + seb2)*stat$ww[rws,]
+      #yy <- (b2 + (n-2)*seb2)*ww[rownames(stat),]
+      yy <- (b2 + seb2)*stat$ww[rws,]
       yy <- colMeans(yy)
       
     }
