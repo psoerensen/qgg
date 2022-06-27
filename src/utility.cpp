197c8c5d66d1d7ec2d5aac0185a79f25683a1a08
Peter Sørensen <au223366@uni.au.dk>
2022-06-27 23:19:37 +0200

    Update utility.cpp

--- src/utility.cpp
+++ src/utility.cpp
@@ -29,42 +29,6 @@ using namespace arma;
 // via the exports attribute we tell Rcpp to make this function
 // available from R
 //
-// [[Rcpp::export]]
-arma::mat rcpparma_hello_world() {
-  arma::mat m1 = arma::eye<arma::mat>(3, 3);
-  arma::mat m2 = arma::eye<arma::mat>(3, 3);
-  
-  return m1 + 3 * (m1 + m2);
-}
-
-
-// another simple example: outer product of a vector, 
-// returning a matrix
-//
-// [[Rcpp::export]]
-arma::mat rcpparma_outerproduct(const arma::colvec & x) {
-  arma::mat m = x * x.t();
-  return m;
-}
-
-// and the inner product returns a scalar
-//
-// [[Rcpp::export]]
-double rcpparma_innerproduct(const arma::colvec & x) {
-  double v = arma::as_scalar(x.t() * x);
-  return v;
-}
-
-
-// and we can use Rcpp::List to return both at the same time
-//
-// [[Rcpp::export]]
-Rcpp::List rcpparma_bothproducts(const arma::colvec & x) {
-  arma::mat op = x * x.t();
-  double    ip = arma::as_scalar(x.t() * x);
-  return Rcpp::List::create(Rcpp::Named("outer")=op,
-                            Rcpp::Named("inner")=ip);
-}
 
 // [[Rcpp::export]]
 arma::mat cp(arma::mat & W) {
