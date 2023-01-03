e8b2d490659b8628210216264465d4dfb8a8edd7
Peter Sørensen <au223366@uni.au.dk>
2023-01-03 19:10:37 +0100

    Update genomic_statistics.R

--- R/genomic_statistics.R
+++ R/genomic_statistics.R
@@ -650,7 +650,7 @@ adjLD <- function(stat = NULL, Glist = NULL, chr=NULL, statistics = "p-value", r
         m <- length(rsidsStat)
         indx1 <- rep(T, m)
         indx2 <- rep(F, m)
-        message(paste("Pruning stat column:", colnames(pstat)[i],"using threshold:",thold))
+        message(paste("Pruning stat column:", i,"using threshold:",thold))
         for (chr in chromosomes) {
           #if (!is.null(Glist)) {
           #message(paste("Pruning chromosome:", chr, "for stat column:", colnames(pstat)[i]))
