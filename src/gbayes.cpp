272507657ab292c6b909f4e42b1c164258adc41d
Peter Sørensen <au223366@uni.au.dk>
2023-01-08 20:41:20 +0100

    Update gbayes.cpp

--- src/gbayes.cpp
+++ src/gbayes.cpp
@@ -1029,7 +1029,7 @@ std::vector<std::vector<double>>  sbayes_spa( std::vector<double> wy,
           lhs1 = ww[i] + vei[i]/vb;
           std::normal_distribution<double> rnorm(rhs1/lhs1, sqrt(vei[i]/lhs1));
           bn = rnorm(gen);
-          if(algo==2 && it<500) {
+          if(algo==2 && it<nburn) {
             bn = (1.0-p0)*bn;
             //bn = (1.0-p0)*(rhs1/lhs1);
           }
@@ -1106,7 +1106,7 @@ std::vector<std::vector<double>>  sbayes_spa( std::vector<double> wy,
           lhs =ww[i]+vei[i]/vbc;
           std::normal_distribution<double> rnorm(rhs/lhs, sqrt(vei[i]/lhs));
           bn = rnorm(gen);
-          if(algo==2 && it<500) {
+          if(algo==2 && it<nburn) {
           bn=0.0;
           for (size_t j = 1; j < gamma.size(); j++) {
             vbc = vb * gamma[j];
